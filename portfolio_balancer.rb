3ff42dd2baead07d093086ff008622db7848f42d
Horncat <94731109+Horncat@users.noreply.github.com>
2021-06-27 18:40:21 +0000

    Hello, world

--- portfolio_balancer.rb
+++ portfolio_balancer.rb
@@ -76,4 +76,5 @@ def display_hash(amounts)
 end
 
 def display_decimal(value)
-  if value > 0 && value < 0.01 || value < 0 && value > -0
+  if value > 0 && value < 0.01 || value < 0 && value > -0.01
+  
