0bf8fb6f2ee9947107238fcb259a2280f7c57bf3
Horncat <94731109+Horncat@users.noreply.github.com>
2022-06-05 20:42:20 +0000

    clean up docs

--- portfolio_balancer.rb
+++ portfolio_balancer.rb
@@ -172,4 +172,4 @@ loop do
         end
 
     m = rate_moving_averages[key].current_value
-    # puts "Key: #{key} | Rel std dev: #{s} | Mean 90: #{dis
+    # puts "Key: #{key} | Rel std dev: #{s} | Mean 90: #{display_decimal
