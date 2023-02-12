5ff2c7d9eaeed03e1e3386bfe2e5e37ac025b741
Horncat <94731109+Horncat@users.noreply.github.com>
2023-02-12 12:08:42 +0000

    114.08+89

--- portfolio_balancer.rb
+++ portfolio_balancer.rb
@@ -199,4 +199,4 @@ loop do
        "Trading effect: #{display_decimal(trading_effect)}"
 
   sek_growth = amounts[:sek] / INITIAL_AMOUNTS[:sek]
-  puts "SEK growth: #{display_decimal(
+  puts "SEK growth: #{display_decimal(sek_g
